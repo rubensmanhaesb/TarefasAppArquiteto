072d9ee394f6993a4c7a124222fc8d5609d5ea01
rubensmanhaesb <rubensmanhaesb@hotmail.com>
2024-02-04 11:16:12 -0300

    first commit

---
+++ TarefasApp.API/Controllers/TarefasController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TarefasApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TarefasController : ControllerBase
+    {
+        /// <summary>
+        /// Serviço para cadastro de tarefas.
+        /// </summary>
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Serviço para atualiação de tarefas.
+        /// </summary>
+        [HttpPut]
+        public async Task<IActionResult> Put()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Serviço para exclusão / inativação de tarefas.
+        /// </summary>
+        [HttpDelete]
+        public async Task<IActionResult> Delete()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Serviço para consulta de tarefas.
+        /// </summary>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok();
+        }
+    }
+}
+
+
+
---
+++ TarefasApp.Integration.Tests/TarefasTest.cs
@@ -0,0 +1,33 @@
+namespace TarefasApp.IntegrationTests
+{
+    /// <summary>
+    /// Testes de integração do ENDPOINT da API de tarefas
+    /// </summary>
+    public class TarefasTest
+    {
+        [Fact(Skip = "Não implementado.")]
+        public async Task PostTarefas_ReturnsSuccessStatusCode()
+        {
+            await Task.CompletedTask;
+        }
+
+        [Fact(Skip = "Não implementado.")]
+        public async Task PutTarefas_ReturnsSuccessStatusCode()
+        {
+            await Task.CompletedTask;
+        }
+
+        [Fact(Skip = "Não implementado.")]
+        public async Task DeleteTarefas_ReturnsSuccessStatusCode()
+        {
+            await Task.CompletedTask;
+        }
+
+        [Fact(Skip = "Não implementado.")]
+        public async Task GetTarefas_ReturnsSuccessStatusCode()
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
+
